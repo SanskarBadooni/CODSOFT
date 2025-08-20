0948a5fce813a3e712857e5954a623b7951bfdc9
SanskarBadooni <88261796+SanskarBadooni@users.noreply.github.com>
2025-08-20 11:38:47 +0530

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def generate_password():
+    length = int(input("Enter the desired password length: "))
+
+    
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    
+    print("\nGenerated Password:", password)
+
+generate_password()
